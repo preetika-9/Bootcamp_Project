63d77d1175f5adb95fea8658b5f3d93718574c9f
preetika-9 <preetikapandey71@gmail.com>
2022-03-09 13:48:20 +0545

    tests done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import "./App.css";
 import React from "react";
 
-
-import { Login } from "./components/organisms/Login";
-
-import { AppRoutes } from "./Routes";
+// import { AppRoutes } from "./Routes";
 import { AddIncome } from "./components/organisms/addIncome";
 import { ListPage } from "./components/organisms/ListPage";
-import Header from "./Routes/Header/Header";
-
+// import Header from "./Routes/Header/Header";
+//import { Login } from "./components/organisms";
 
 function App() {
   return (
     <>
+      {/* <Login />
       <AppRoutes />
-      <Header />
+      <Header />*/}
       <ListPage />
       <AddIncome />
     </>
